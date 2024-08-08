80d4fec31715571ac16e4d02dc21a412af7f1795
Fatoumata32 <f.ndiaye@alustudent.com>
2024-08-08 19:18:49 +0000

    everything

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using requests"""
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
 
+
 if __name__ == "__main__":
-    """Fetches https://intranet.hbtn.io/status using requests"""
-    try:
-        # Send GET request
-        r = requests.get('https://intranet.hbtn.io/status')
-        
-        # Ensure we handle redirection correctly (if applicable)
-        r.raise_for_status()  # Raise an exception for HTTP errors
-        
-        # Print response details
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.text.strip()))
-        
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    """fetches https://intranet.hbtn.io/status using requests"""
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
